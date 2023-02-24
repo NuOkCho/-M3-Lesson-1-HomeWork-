99eb66a7389de59fcfcf268bc049eb343d0952f8
Ahmed Muvaza <makbookicloud312@mail.ru>
2023-02-24 16:25:09 +0600

    a

--- (M3)Lesson-1(HomeWork)/ViewController.swift
+++ (M3)Lesson-1(HomeWork)/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var input: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func signIn(_ sender: Any) {
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if login.text?.isEmpty ?? true {
+            login.layer.borderWidth = 2
+            login.layer.borderColor = UIColor.red.cgColor
+        }
+         if password.text?.isEmpty ?? true {
+            password.layer.borderWidth = 2
+            password.layer.borderColor = UIColor.red.cgColor
+        }
+      }
     }
-
-
-}
-
